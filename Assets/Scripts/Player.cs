a6496fb8250a22b35e899d91adb09592042f21db
Ege Can Sarı <89903168+E9ex@users.noreply.github.com>
2022-09-18 00:24:54 +0300

    secondtrial

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject cam;
+    public GameObject vectorforward;
+    public GameObject vectorback;
+    public Rigidbody rb;
+    private Touch touch;
+    [Range(20,40)]// böyle yaparsak slider geliyor.
+    public int speed;
 
-    // Update is called once per frame
-    void Update()
+    public int forwardspeed;
+    // Start is called before the first frame update
+    public void FixedUpdate()
     {
+        if (variables.firsttouch==1)//dokundgumuzda böyle olsun yani... (bu arada Variables değil o.Aslında dosya ismi.)
+        {
+            transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
+            cam.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
+            vectorforward.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
+            vectorback.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
+        }
+
         
+        
+
+
+
+            if (Input.touchCount>0)
+        {
+            touch = Input.GetTouch(0);//ilk dokunan parmagı aliyoruz.
+            if (touch.phase==TouchPhase.Began)
+            {
+                variables.firsttouch = 1;
+            }
+            if (touch.phase==TouchPhase.Moved)
+            {
+               /* bak kardeşim şimdi burda transform ilerde cok buyuk hatalar alabilirm böyle yapınca kutuların içinden geçiyor geçmemesi lazim // transform.position*/
+               rb.velocity=new Vector3(touch.deltaPosition.x*speed,transform.position.y,touch.deltaPosition.y*speed);
+                // böyle yaparsam yukarda yazdıgım gibi küplerin içinden geçmez.
+            }
+
+            if (touch.phase==TouchPhase.Ended)
+            {
+                rb.velocity = Vector3.zero;// zero neydi hepsini sıfır yapıyor. hız tamamen kesilsin diye böyle yazdık.
+            }
+
+            if (touch.phase==TouchPhase.Stationary)
+            {
+                rb.velocity = Vector3.zero;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/variables.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    public static int firsttouch = 0;//ekrana dokundgumuzda bu sayı artsın 
+}
