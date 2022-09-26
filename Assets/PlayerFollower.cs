8b073260de4971b6b083c0ef0a4d084badd3be5b
Timucin Topal <timucintopal@gmail.com>
2022-09-26 21:53:23 +0300

    [Solved] Ball Issue

---
+++ Assets/PlayerFollower.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    private Vector3 selfPosition;
+
+    [SerializeField] private Transform player;
+    
+    
+    private void Awake()
+    {
+        selfPosition = transform.position;
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = selfPosition + player.position;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,24 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Resources;
-using System.Runtime.InteropServices;
-using System.Transactions;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Windows.Speech;
 
 public class Player : MonoBehaviour
 {
+    [Header("STATUS")] 
+    [SerializeField] private bool isSaglam = true; // STATE
+    
+    //STATES
+    //IDLE
+    //MOVE/WALK
+    //RUN
+    
+    //FULLHEALTH = 100
+    //INJURED    <0 && <100
+    //DIED = 0
+
+    [Space] [Header("INIT")] 
+    [SerializeField] private Collider selfCollider;
+    [SerializeField] private GameObject selfModel;
+    [Space]
+    
     public UIManager uimanager;
     public camerashake camesahake;
     public GameObject cam;
@@ -29,43 +38,43 @@ public class Player : MonoBehaviour
 
     public void Update()
     {
-        if (variables.firsttouch ==1) //dokundgumuzda böyle olsun yani... (bu arada Variables değil o.Aslında dosya ismi.)
+        if(isSaglam)
         {
-            transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
-            cam.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
-            vectorforward.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
-            vectorback.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
-        }
-
-
-
-
-
-
-        if (Input.touchCount > 0)
-        {
-            touch = Input.GetTouch(0); //ilk dokunan parmagı aliyoruz.
-            if (touch.phase == TouchPhase.Began)
+            if (variables.firsttouch ==
+                1) //dokundgumuzda böyle olsun yani... (bu arada Variables değil o.Aslında dosya ismi.)
             {
-                variables.firsttouch = 1;
+                transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
+                // cam.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
+                vectorforward.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
+                vectorback.transform.position += new Vector3(0, 0, forwardspeed * Time.deltaTime);
             }
 
-            if (touch.phase == TouchPhase.Moved)
+            if (Input.touchCount > 0)
             {
-                /* bak kardeşim şimdi burda transform ilerde cok buyuk hatalar alabilirm böyle yapınca kutuların içinden geçiyor geçmemesi lazim // transform.position*/
-                rb.velocity = new Vector3(touch.deltaPosition.x * speed * Time.deltaTime, transform.position.y,
-                    touch.deltaPosition.y * speed * Time.deltaTime);
-                // böyle yaparsam yukarda yazdıgım gibi küplerin içinden geçmez.
-            }
+                touch = Input.GetTouch(0); //ilk dokunan parmagı aliyoruz.
+                if (touch.phase == TouchPhase.Began)
+                {
+                    variables.firsttouch = 1;
+                }
 
-            if (touch.phase == TouchPhase.Ended)
-            {
-                rb.velocity = Vector3.zero; // zero neydi hepsini sıfır yapıyor. hız tamamen kesilsin diye böyle yazdık.
-            }
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    /* bak kardeşim şimdi burda transform ilerde cok buyuk hatalar alabilirm böyle yapınca kutuların içinden geçiyor geçmemesi lazim // transform.position*/
+                    rb.velocity = new Vector3(touch.deltaPosition.x * speed * Time.deltaTime, transform.position.y,
+                        touch.deltaPosition.y * speed * Time.deltaTime);
+                    // böyle yaparsam yukarda yazdıgım gibi küplerin içinden geçmez.
+                }
 
-            if (touch.phase == TouchPhase.Stationary)
-            {
-                rb.velocity = Vector3.zero;
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    rb.velocity =
+                        Vector3.zero; // zero neydi hepsini sıfır yapıyor. hız tamamen kesilsin diye böyle yazdık.
+                }
+
+                if (touch.phase == TouchPhase.Stationary)
+                {
+                    rb.velocity = Vector3.zero;
+                }
             }
         }
     }
@@ -73,21 +82,32 @@ public class Player : MonoBehaviour
     public GameObject[] fractureItems;
     public void OnCollisionEnter(Collision hit)
     {
-        if (hit.gameObject.CompareTag("obstacles1"))
-        {
-            gameObject.transform.GetChild(0).gameObject.SetActive(false);
-           camesahake.camerashakecall();
-            uimanager.StartCoroutine("whiteeffect");
-            
-            foreach (GameObject item in fractureItems)
-            {
-                
-                item.GetComponent<SphereCollider>().enabled = true;
-               
-                item.GetComponent<Rigidbody>().isKinematic = false;
-            }
-        }
+        // if(isCrashed)
+        //     return;
         
+       if(isSaglam == true)
+       {
+           if (hit.gameObject.CompareTag("obstacles1"))
+           {
+               isSaglam = false;
+               rb.isKinematic = true;
+               selfCollider.enabled = false;
+               selfModel.SetActive(false);
+
+               gameObject.transform.GetChild(0).gameObject.SetActive(false);
+               camesahake.camerashakecall();
+               uimanager.StartCoroutine("whiteeffect");
+
+               foreach (GameObject item in fractureItems)
+               {
+                   item.GetComponent<Collider>().enabled = true;
+
+                   item.GetComponent<Rigidbody>().isKinematic = false;
+
+                   item.transform.SetParent(null);
+               }
+           }
+       }
     }
 }
 
